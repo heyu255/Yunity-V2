657f9b4dd51f528f05b9bdf888e108c46cf9e428
heyu255 <yus2@andrew.cmu.edu>
2025-12-21 21:55:46 -0800

    first commit

---
+++ yunity-studio/src/app/error/page.tsx
@@ -0,0 +1,9 @@
+export default function ErrorPage() {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-xl font-bold">Authentication Error</h1>
+        <p>Something went wrong. Please check your credentials and try again.</p>
+        <a href="/login" className="mt-4 text-blue-500 underline">Back to Login</a>
+      </div>
+    )
+  }
