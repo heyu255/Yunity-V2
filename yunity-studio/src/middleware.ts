0a22d03b51112f75f3156f48d5930e60fcc13ac3
heyu255 <hey.yu2025@gmail.com>
2025-12-23 22:43:58 -0800

    stripe fix

--- yunity-studio/src/middleware.ts
+++ yunity-studio/src/middleware.ts
@@ -42,7 +42,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    
     '/((?!api/webhooks/stripe|_next/static|_next/image|favicon.ico).*)',
   ],
 }
